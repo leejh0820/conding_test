119ce3df5c70fb288153d04216bdb40865e52473
Jeonghyun Lee <96366537+leejh0820@users.noreply.github.com>
2025-12-18 17:35:57 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.py
@@ -1,53 +1,44 @@
 import sys
-input = sys.stdin.read
 
-def solve():
-    data = input().split()
-    if not data:
-        return
-    
-    N = int(data[0])
-    M = int(data[1])
-    x = int(data[2]) 
-    y = int(data[3]) 
-    K = int(data[4])
-    
-    ptr = 5
-    board = []
-    for _ in range(N):
-        board.append(list(map(int, data[ptr:ptr+M])))
-        ptr += M
-    
-    commands = list(map(int, data[ptr:]))
-    
-    dice = [0] * 6
 
-    dx = [0, 0, 0, -1, 1]
-    dy = [0, 1, -1, 0, 0]
+input = sys.stdin.readline
+
+n, m, x, y, k = map(int, input().split())
+
+
+board = [list(map(int, input().split())) for _ in range(n)]
+commands = list(map(int, input().split()))
+
+
+dice = [0] * 6
+
+dx = [0, 0, 0, -1, 1]
+dy = [0, 1, -1, 0, 0]
+
+for cmd in commands:
+    nx = x + dx[cmd]
+    ny = y + dy[cmd]
+
+    if not (0 <= nx < n and 0 <= ny < m):
+        continue  
     
-    for cmd in commands:
-        nx, ny = x + dx[cmd], y + dy[cmd]
+    if cmd == 1: 
+        dice[0], dice[2], dice[3], dice[5] = dice[3], dice[0], dice[5], dice[2]
+    elif cmd == 2: 
+        dice[0], dice[2], dice[3], dice[5] = dice[2], dice[5], dice[0], dice[3]
+    elif cmd == 3: 
+        dice[0], dice[1], dice[4], dice[5] = dice[4], dice[0], dice[5], dice[1]
+    elif cmd == 4: 
+        dice[0], dice[1], dice[4], dice[5] = dice[1], dice[5], dice[0], dice[4]
         
-        if not (0 <= nx < N and 0 <= ny < M):
-            continue
-        
-        if cmd == 1: 
-            dice[0], dice[2], dice[3], dice[5] = dice[3], dice[0], dice[5], dice[2]
-        elif cmd == 2: 
-            dice[0], dice[2], dice[3], dice[5] = dice[2], dice[5], dice[0], dice[3]
-        elif cmd == 3: 
-            dice[0], dice[1], dice[4], dice[5] = dice[4], dice[0], dice[5], dice[1]
-        elif cmd == 4: 
-            dice[0], dice[1], dice[4], dice[5] = dice[1], dice[5], dice[0], dice[4]
-            
-            
-        if board[nx][ny] == 0:
-            board[nx][ny] = dice[5]
-        else:
-            dice[5] = board[nx][ny]
-            board[nx][ny] = 0
-            
-        x, y = nx, ny
-        print(dice[0])
+       
+    
+    if board[nx][ny] == 0:
+        board[nx][ny] = dice[5]
+    else:
 
-solve()
+        dice[5] = board[nx][ny]
+        board[nx][ny] = 0
+
+    x, y = nx, ny
+    print(dice[0])
