ef4f75f74e7996c86ff5ee90de99bc3b0cb4b900
Jeonghyun Lee <96366537+leejh0820@users.noreply.github.com>
2026-03-09 16:13:26 +0900

    [Gold V] Title: 신기한 소수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -0,0 +1,24 @@
+import sys
+sys.setrecursionlimit(10000)
+N = int(sys.stdin.readline())
+
+def isPrime(num):
+    for i in range(2, int(num**0.5 + 1)):
+        if num % i == 0:
+            return False
+    return True
+
+def DFS(number):
+    if len(str(number)) == N:
+        print(number)
+    else:
+        for i in range(1, 10):
+            if i % 2 == 0:
+                continue
+            if isPrime(number*10 + i):
+                DFS(number*10+i)
+
+DFS(2)
+DFS(3)
+DFS(5)
+DFS(7)
