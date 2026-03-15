3e6b9eae7b9c9a481f7fdfd73835df62f6ffb8b1
Jeonghyun Lee <96366537+leejh0820@users.noreply.github.com>
2026-03-15 22:56:30 +0900

    [Gold IV] Title: 오큰수, Time: 1044 ms, Memory: 138596 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,15 @@
+import sys
+N = int(sys.stdin.readline())
+A = list(map(int, sys.stdin.readline().split()))
+stack = []
+answer = [0] * N
+
+for i in range(N):
+    while stack and A[stack[-1]] < A[i]:
+        answer[stack.pop()] = A[i]
+    stack.append(i)
+while stack:
+    answer[stack.pop()] = -1
+
+for i in range(N):
+    sys.stdout.write(str(answer[i]) + ' ')
