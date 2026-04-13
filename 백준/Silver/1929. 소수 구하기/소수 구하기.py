e473d2c350199581ffc89e1a609cbffbfc64bbdf
Jeonghyun Lee <96366537+leejh0820@users.noreply.github.com>
2026-04-13 10:20:05 +0900

    [Silver III] Title: 소수 구하기, Time: 484 ms, Memory: 72932 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,19 @@
+import sys
+import math
+
+M, N = map(int, sys.stdin.readline().split())
+
+A = [0] * (N + 1)
+
+for i in range(2, N + 1):
+    A[i] = i
+
+for i in range(2, int(math.sqrt(N)) + 1):
+    if A[i] == 0:
+        continue
+    for j in range(i + i, N + 1, i):
+        A[j] = 0
+    
+for i in range(M, N + 1):
+    if A[i] != 0:
+        print(A[i])
