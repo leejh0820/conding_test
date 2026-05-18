832036a18678ef0f40a479c278308bbf283c938d
Jeonghyun Lee <96366537+leejh0820@users.noreply.github.com>
2026-05-18 10:27:51 +0900

    [level 2] Title: 프로세스, Time: 0.61 ms, Memory: 9.26 MB -BaekjoonHub

---
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def solution(priorities, location):
+    answer = 0
+    queue = deque([(i, p) for i, p in enumerate(priorities)])
+    
+    while queue:
+        current = queue.popleft()
+        
+        if any(current[1] < q[1] for q in queue):
+            queue.append(current)
+        else:
+            answer += 1
+            
+            if current[0] == location:
+                return answer
