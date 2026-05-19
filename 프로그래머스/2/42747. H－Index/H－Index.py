807a8d9bca2903fc160b65c1a2adb2661cd214a8
Jeonghyun Lee <96366537+leejh0820@users.noreply.github.com>
2026-05-19 09:19:15 +0900

    [level 2] Title: H-Index, Time: 0.09 ms, Memory: 9.13 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -0,0 +1,8 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    
+    for i, citation in enumerate(citations):
+        if citation < i + 1:
+            return i
+
+    return len(citations)
